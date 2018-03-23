4923cd563f45b5199b3a9018c7728f607a3cc942
Mina Slater <slater.mina@gmail.com>
2018-03-23 18:27:29 -0500

    adds files for controllers and each api route

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,17 @@
 const router = require("express").Router();
 const userRoutes = require("./user");
+const businessRoutes = require("./business");
+const commentRoutes = require("./comment");
+const flightRoutes = require("./flight");
+const profileRoutes = require("./profile");
+const tripRoutes = require("./trip");
 
 // Book routes
 router.use("/users", userRoutes);
+router.use("/business", businessRoutes);
+router.use("/comment", commentRoutes);
+router.use("/flight", flightRoutes);
+router.use("/profile", profileRoutes);
+router.use("/trip", tripRoutes);
 
 module.exports = router;
