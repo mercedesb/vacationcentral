435bb5cbe172dc80ca1a882360fc0838b2a9d2b6
Mina Slater <slater.mina@gmail.com>
2018-04-02 13:12:30 -0500

    changes col size of trip and display panels

--- client/src/components/HeaderPanel/HeaderPanel.js
+++ client/src/components/HeaderPanel/HeaderPanel.js
@@ -3,7 +3,7 @@ import "./HeaderPanel.css";
 import { Row, Col } from 'react-bootstrap';
 
 const HeaderPanel = props => (
-  <Row xs={12} className="header-panel">
+  <Row xs={12} md={2} className="header-panel">
     <Col xs={6}>
       <p className="hp-text"> Vacation Central</p>
     </Col>
