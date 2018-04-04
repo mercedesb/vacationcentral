036ed5c7cadc9e227fb7abe565db5677b3aeda6e
Mina Slater <slater.mina@gmail.com>
2018-04-04 00:01:06 -0400

    resolves merge conflicts and saves usersController

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -21,13 +21,6 @@ module.exports = {
     db.User
       .create(req.body)
       .then(data => res.redirect(307, "/api/users/login"))
-<<<<<<< HEAD
-      .catch(err => {
-        console.log("err", err.errors[0].message);
-        res.status(422).json(err)
-        });
-=======
       .catch(err => res.status(422).json(err.errors[0].message));
->>>>>>> 570338b7617e0da884f4c0816d160c167bfad591
   }
 };
